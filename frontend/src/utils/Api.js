4b60f83a83e6c7e9320cd55a063058757b4d8da4
Aleksey Shemyakin <carl0slit0s@yndex.ru>
2022-07-20 22:27:59 +0500

    правки

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -136,7 +136,7 @@ class Api {
   }
 }
 
-export const api = new Api('http://api.project-mesto72.nomoredomains.xyz', {
+export const api = new Api('api.project-mesto72.nomoredomains.xyz', {
   headers: {
     'Content-Type': 'application/json',
   },
